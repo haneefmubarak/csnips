e0dba1a574b9084599cba23e8ed4bd5979fa521b
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2014-05-06 01:41:59 -0700

    hashing header

---
+++ hash/hash.h
@@ -0,0 +1,47 @@
+// snips/hash.h
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// hash library headers
+#include <blake2.h>
+#include <openssl/sha.h>
+#include <openssl/md5.h>
+
+typedef enum {
+	SNIPS_HASH_INVALID_H	= -1,
+	SNIPS_HASH_DEFAULT_H	= 0,
+	SNIPS_HASH_BLAKE2_H	= 1,
+	SNIPS_HASH_SHA1_H	= 2,
+	SNIPS_HASH_MD5_H	= 3
+} snips_hash_h_t;
+
+typedef enum {
+	SNIPS_HASH_INVALID_M	= -1,
+	SNIPS_HASH_DEFAULT_M	= 0,
+	SNIPS_HASH_FORCE_M	= 1,
+	SNIPS_HASH_MINLEN_M	= 2,
+	SNIPS_HASH_MAXLEN_M	= 3,
+	SNIPS_HASH_NEARLEN_M	= 4
+} snips_hash_m_t;
+
+typedef struct {
+	uint8_t *in;	// input byte array
+	uint64_t len;	// length of ^^^^^^
+	snips_hash_h_t hash;	// which hash to use
+	snips_hash_m_t mode;	// which hash selection heuristic to use
+	struct {
+		uint8_t len;	// preferred output length
+	} prefer;
+} snips_hash_in_t;
+
+typedef struct {
+	uint8_t *out;	// output byte array
+	snips_hash_h_t hash;	// which hash was used
+	snips_hash_m_t mode;	// which hash selection heuristic was used
+	int16_t len;	// length of ^^^^^^^
+	int16_t err;	// treat zero as valid, all else is an error
+} snips_hash_out_t;
+
+snips_hash_out_t snips_hash_f (snips_hash_in_t);
